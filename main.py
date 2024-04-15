d296052b1b6eea3952b3a514f1151038f9017513
ergwat <denis.anisimov@gmail.com>
2024-04-15 16:13:50 +0300

    + комментарии, -мелкие ошибки

--- main.py
+++ main.py
@@ -25,18 +25,22 @@ color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 running=True
 while running:
+    # заливаем экран случайным цветом из рандомной константы
     screen.fill(color)
+    # размещаем мишень в случайные координаты
     screen.blit(target_image, (target_x, target_y))
-    for event in pygame.event.get():
-        if __name__ == '__main__':
-            match event.type:
-                case pygame.QUIT: running = False
-                case pygame.MOUSEBUTTONDOWN:
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    if target_x < mouse_x < target_x + target_width and target_y <mouse_y < target_y + target_height:
-                        target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                        target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+    # основной цикл, отслеживающий любые события
+    for event in pygame.event.get():
+        match event.type:
+            case pygame.QUIT: running = False
+            case pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if target_x < mouse_x < target_x + target_width and target_y <mouse_y < target_y + target_height:
+                    target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                    target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    # смена кадра
     pygame.display.update()
 
 
